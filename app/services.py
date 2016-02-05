b53d74391ebda2181e94a6fe8fb52aa765114fe6
Chase Conklin <chase@conklins.net>
2016-02-05 11:01:07 -0500

    added exception handling for notifications (#7)

--- app/services.py
+++ app/services.py
@@ -8,7 +8,11 @@ class Notifier(object):
         self.threshold = threshold
 
     def notify(self):
-        self.threshold.triggered_at = datetime.datetime.now()
-        requests.post("http://{}/api/notify".format(PLANT_DATABASE),
-                      data={'plant': self.threshold.plant.name})
-        self.threshold.save()
+        try:
+            requests.post("http://{}/api/notify".format(PLANT_DATABASE),
+                          data={'plant': self.threshold.plant.name})
+        except requests.exceptions.ConnectionError:
+            pass
+        else:
+            self.threshold.triggered_at = datetime.datetime.now()
+            self.threshold.save()
--- test/test_services.py
+++ test/test_services.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import app.services as services
 from app.config import PLANT_DATABASE
 
-@mock.patch("app.services.requests")
+@mock.patch("app.services.requests.post")
 class TestNotifier(unittest.TestCase):
 
     def setUp(self):
@@ -16,24 +16,32 @@ class TestNotifier(unittest.TestCase):
         self.notification_threshold = mock.Mock(
             name="notification_threshold",
             plant=plant,
-            save=mock.Mock())
+            save=mock.Mock(),
+            triggered_at=dt(2011, 01, 1))
         self.notifier = services.Notifier(self.notification_threshold)
 
     @mock.patch("app.services.datetime.datetime")
-    def test_sets_triggered_at_on_threshold(self, datetime, requests):
+    def test_sets_triggered_at_on_threshold(self, datetime, post):
         datetime.now.return_value = dt(2016, 02, 11)
         self.notifier.notify()
         self.assertEqual(self.notification_threshold.triggered_at,
                          dt(2016, 02, 11))
         self.notification_threshold.save.assert_called_with()
 
-    def test_sends_notification(self, requests):
+    def test_sends_notification(self, post):
         self.notifier.notify()
-        requests.post.assert_called_with(
+        post.assert_called_with(
             "http://{}/api/notify".format(PLANT_DATABASE),
             # later worry about having the correct headers
             data={'plant': 'Hydrangea'}
         )
+
+    def test_does_not_set_triggered_if_notification_isnt_sent(self, post):
+        post.side_effect = services.requests.exceptions.ConnectionError
+        self.notifier.notify()
+        self.assertEqual(self.notification_threshold.triggered_at,
+                         dt(2011, 01, 1))
+        self.notification_threshold.save.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
