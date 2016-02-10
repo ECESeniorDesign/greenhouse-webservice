dae60e29c05ce8f4010a37a9e5a81c754a8e44bf
Chase Conklin <chase@conklins.net>
2016-02-09 22:04:30 -0500

    add delete to settings

--- app/webservice.py
+++ app/webservice.py
@@ -15,6 +15,25 @@ loader = jinja2.FileSystemLoader(template_dir)
 environment = jinja2.Environment(loader=loader)
 
 app = flask.Flask(__name__)
+
+from werkzeug import url_decode
+
+class MethodRewriteMiddleware(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        if 'METHOD_OVERRIDE' in environ.get('QUERY_STRING', ''):
+            args = url_decode(environ['QUERY_STRING'])
+            method = args.get('__METHOD_OVERRIDE__')
+            if method:
+                method = method.encode('ascii', 'replace')
+                environ['REQUEST_METHOD'] = method
+        return self.app(environ, start_response)
+
+app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
+
 socketio = SocketIO(app, async_mode='eventlet')
 app.secret_key = config.SECRET_KEY
 router = router.Router(app)
